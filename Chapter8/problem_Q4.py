edc41aa83a110303d4ea2c20daae9f8bc25485ea
Bhushan1834 <bhushantarte44@gmail.com>
2026-02-16 20:55:10 +0530

    Create problem_Q4.py

---
+++ Chapter8/problem_Q4.py
@@ -0,0 +1,18 @@
+'''Q4.Write a recursive function to calculate the sum of first n natural numbers.
+sum (1) = 1
+sum (2) = 1 + 2
+sum (3) = 1 + 2 + 3
+sum (4) = 1 + 2 + 3 + 4
+sum (5) = 1 + 2 + 3 + 4 + 5
+
+sum (n) = 1 + 2 + 3 + 4.......n -1 + n
+sum (n) = sum (n-1) + n
+
+
+'''
+# base condition use hota hai recursion infinite ulta na jaye
+def sum(n):
+    if(n==1):
+        return 1
+    return sum(n-1) + n
+print(sum(4))
