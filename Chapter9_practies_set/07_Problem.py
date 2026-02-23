9055c5d987d3bff3abfe91ba8b0241dde8ed165f
Bhushan1834 <bhushantarte44@gmail.com>
2026-02-23 10:00:38 +0530

    Create 07_Problem.py

---
+++ Chapter9_practies_set/07_Problem.py
@@ -0,0 +1,16 @@
+'''
+Q7.Write a program to find out the line number where python is present from question 6.
+'''
+with open("log.txt") as f:
+    lines = f.readlines()
+
+lineno = 1
+
+for line in lines:
+    if("python" in line):
+        print(f"Yes python is present. Line no: {line}")
+        break
+    lineno += 1
+
+else:
+    print("No Python is not present")    
