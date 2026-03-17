03b1a57df60897c431aeee7ff07a7a866f819c75
Bhushan1834 <bhushantarte44@gmail.com>
2026-03-17 22:00:08 +0530

    Create 02_instance_vs_class_attribute.py

---
+++ Chapter10/02_instance_vs_class_attribute.py
@@ -0,0 +1,8 @@
+class Employee:
+    language = "py" # This is a class attribute
+    salary = 1900000
+
+bhushan = Employee()
+bhushan.language= "javaScript" #This is an object(instance) attribute 
+print(bhushan.language, bhushan.salary)
+
