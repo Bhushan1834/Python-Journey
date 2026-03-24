ed749492fbb21bbdcfab9c79e8e3585d63930240
Bhushan1834 <bhushantarte44@gmail.com>
2026-03-24 22:54:35 +0530

    Create 04_init_constructor.py

---
+++ Chapter10/04_init_constructor.py
@@ -0,0 +1,13 @@
+# init() is a special method which is first run as soonas the object is created 
+# init() method is also known as constructor
+# it takes self argument and can also take further arguments
+
+#Eg
+class Employee:
+    def __init__(self, name):
+        self.name=name
+        def getSalary(self):
+            ...
+bhushan = Employee("Bhushan")
+
+            
