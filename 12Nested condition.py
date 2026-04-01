06b7a501b081e936a78c187ef0cbeb8cf9ee84e3
Bhushan1834 <bhushantarte44@gmail.com>
2026-04-01 20:07:03 +0530

    Create 12Nested condition.py

---
+++ 12Nested condition.py
@@ -0,0 +1,10 @@
+x = 20
+if x > 5:
+    print("x is greater than 5")
+    if x %2 == 0:
+        print("x is also even ")
+    else :
+        print("x is odd")
+else:
+    print("x is less than 5,so not calculating even or odd ")
+    
