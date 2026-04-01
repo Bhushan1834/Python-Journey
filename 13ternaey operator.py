a30735e43006f3439eda7cbdcea23db527c7f538
Bhushan1834 <bhushantarte44@gmail.com>
2026-04-01 20:07:13 +0530

    Create 13ternaey operator.py

---
+++ 13ternaey operator.py
@@ -0,0 +1,10 @@
+# num = 5
+# if num >= 0:
+#     print("Number is positive ")
+# else:
+#     print("Number is negative ")
+
+# ternary operator 
+num = 5
+result = "Positive "if num >=0 else "Negative "
+print ("Number is ", result )
