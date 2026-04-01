373bf559c4a688147aad91771f1078cec65d8f08
Bhushan1834 <bhushantarte44@gmail.com>
2026-04-01 20:07:07 +0530

    Create 14match and case satement .py

---
+++ 14match and case satement .py
@@ -0,0 +1,22 @@
+#  match an case 
+day_num = 10
+match day_num :
+    case 1:
+        print("Today is Monday ")
+    case 2:
+        print("Todya is Tuesday ")    
+    case 3:
+        print("Todya is wednesday ") 
+    case 4:
+        print("Todya is Thursday ") 
+    case 5:
+        print("Todya is Friday  ") 
+    case 6:
+        print("Todya is  Satutday ") 
+    case 7:
+        print("Todya is  Sunday ") 
+    case _ :
+        print("Invalid day number ")
+
+    
+
