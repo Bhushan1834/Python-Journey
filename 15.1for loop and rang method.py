f9d527007076a206cef3bda16a3aaa80bde2f2a5
Bhushan1834 <bhushantarte44@gmail.com>
2026-04-01 20:07:27 +0530

    Create 15.1for loop and rang method.py

---
+++ 15.1for loop and rang method.py
@@ -0,0 +1,21 @@
+# fruits = ["apple","banana","cherry","date","elderberry"]
+# for fruit in fruits :
+#     print(fruit)
+
+# name = "Bhushan Tarte"
+# for ch in name :
+#     print(ch,end=" ")    
+
+#range function
+ 
+# for i in range(5):
+#     print(i)
+
+# for i in range(2,7):
+#     print(i)
+
+# for b in range(3,10,3):
+#     print(b)
+
+for i in range(12,0,-3):
+    print(i)
