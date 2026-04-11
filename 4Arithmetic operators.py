4e54f0f0461a0e437a8f8e8c0cf1f5d1c8a4b5ea
Bhushan1834 <bhushantarte44@gmail.com>
2026-04-11 23:16:42 +0530

    Create 4Arithmetic operators.py

---
+++ 4Arithmetic operators.py
@@ -0,0 +1,22 @@
+# num1 = 10
+# num2 = 20
+
+# print(num2+num1)
+# print(num2-num1)
+# print(num2*num1)
+# print(num2/num1)
+# print(23/3)
+# print(23//3)
+# print(23%3)
+
+# num = int(input("Enter the number"))
+# print(num%2 ==0)
+# pos_inf = float ("inf")
+# print(pos_inf)
+
+# neg_inf = float("-inf")
+# print(neg_inf)
+# nan = float("nan")
+# print(nan)  
+
+
