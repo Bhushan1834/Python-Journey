7aafaa7ca71c303f87a407fefa6e038c945a8463
Bhushan1834 <bhushantarte44@gmail.com>
2026-04-11 23:16:33 +0530

    Create Assignment oprator.py

---
+++ Assignment oprator.py
@@ -0,0 +1,27 @@
+name ="Bhushan"
+x = 5
+x = x+5
+print(x)
+
+#incement and assin
+
+x +=5
+print(x)
+
+x -=3
+print(x)
+
+x*=2
+print(x)
+
+x/=4
+print(x)
+
+x%=3
+print(x)
+
+x//=4
+print(x)
+
+x<=1
+print(x)
