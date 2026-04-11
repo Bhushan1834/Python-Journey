8eba3e2ddab35dfbb2ddbe7e99d294c279587ada
Bhushan1834 <bhushantarte44@gmail.com>
2026-04-11 23:16:56 +0530

    Create if,else and elifcondition.py

---
+++ if,else and elifcondition.py
@@ -0,0 +1,33 @@
+# x = int(input("Enter the number "))
+# if x>15: 
+#     print("Nmuber is greater then 15 ")
+# elif x==15 :
+#     print("Number is equal to 15 ")    
+# else  :
+#     print("Number is less then 15 ")
+
+marks= int(input("Enter the marks "))
+
+if  marks>90 :
+    print("A+ ","Nmuber is greater then 90")
+elif marks==90 :
+    print("A","number is equal to 90")
+else :
+    print("Number is less than 90")    
+if marks >=80 :
+    print("B+","Nmuber is greater then 80")
+elif marks ==80 :
+    print("B")
+else :
+      print("Number is less than 90")  
+if marks >= 70 :
+    print("c+","Nmuber is greater then 70")
+elif marks ==70 :
+    print("c")
+
+if marks >=60 :
+    print("D+","Nmuber is greater then 60")
+elif marks ==60 :
+    print("D")
+else :
+    print("study hard")   
