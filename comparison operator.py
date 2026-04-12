f13e7325139d82980b3a88c6cd37785f0103eeb2
Bhushan1834 <bhushantarte44@gmail.com>
2026-04-12 22:23:11 +0530

    Create comparison operator.py

---
+++ comparison operator.py
@@ -0,0 +1,18 @@
+# num1 = 10 
+# num2 = 20 
+# print(num1 == num2)
+
+# print(num1 != num2)
+
+# print(num1 < num2)
+
+# print(num1 <= num2)
+# print(num1 > num2)
+# print(num1 >= num2)
+
+x = float('nan')
+y = 5
+z = float('nan')
+print(x == y)
+
+print(z == x)
