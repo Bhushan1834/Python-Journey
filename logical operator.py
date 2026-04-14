37462718c78deffc026dffca297bc3aab39da747
Bhushan1834 <bhushantarte44@gmail.com>
2026-04-14 09:36:43 +0530

    Create logical operator.py

---
+++ logical operator.py
@@ -0,0 +1,18 @@
+# print(True  and False)
+
+# print(True and False)
+# print(False and True)
+# print(False and False)
+
+# print(True or True )
+# print(True or False)
+# print(False or True )
+# print(False or False )
+
+# print(not True)
+
+#print(not 5<7)
+
+print(True ^ False)
+
+print(True ^ True )
