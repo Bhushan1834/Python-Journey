26b9d8d107a5db03a2f4ff27627383d5c764bff8
Bhushan1834 <bhushantarte44@gmail.com>
2026-04-17 22:58:04 +0530

    Create practice session.py
    added

---
+++ practice session.py
@@ -0,0 +1,84 @@
+# def firstFunction():
+#     print("hello world")
+# firstFunction()
+
+# def add(a,b):
+#     sum = a+b
+#     return sum
+# print(add(10,20))
+
+# b = 106 
+# def func():
+#      b = 104
+#      print(b)
+# func()
+# print(b)
+
+
+# def greet(name, message =" ka empaier" ):
+#     print(name,message)
+# greet("bhushan")
+
+# def info(message, name , age ):
+#     print(message,"your age is", age,name  )
+# info(name="bhushan" ,age = 21, message=" hellow")
+
+# def add_value(x,y,z,b) :
+#     print("x",y)
+#     print("y",x)
+#     print("z",b)
+#     print("b",z)
+#     print(x+y+z+b)
+# add_value(6,8,7,5)
+
+# def func(a,b,*args):
+#     print(a)
+#     print(b)
+#     print(args)
+#     print(*args)
+# func(6,7,8,9,1)
+
+# def func(*args,a,b):
+#     print(args)
+#     print(*args)
+#     print(a)
+#     print(b)
+# func(4,5,6,a =1,b=2)
+
+# def sum_numbers(*args):
+#     print(type(args))
+#     print(*args)
+#     sum = 0
+#     for num in args:
+#      sum+= num 
+#     return sum 
+# print(sum_numbers(1,2,3,4,5,))
+
+# def disply_info(**kwargs):
+#     print(kwargs)
+#     print(type(kwargs))
+#     for key,value in kwargs.iteam():
+#      print(key,"->",value)
+# disply_info(name ="bhushan",age=99,city="pune")
+     
+# def func(a,b,*args,**kwargs):
+#     print(a)
+#     print(b)
+#     print(args)
+#     print(*args)
+#     print(kwargs)
+# func(2,4,5,6,6,name= "bhushan",age= 21,city="pune")
+
+# def func1(**kwargs,a,b):
+#     print(kwargs)
+#     print(a)
+#     print(b)
+# func1(name="bhushan",age = 21,2,4,)
+
+# for i in "LOVE":
+#     if i == "V":
+#         print(i,end="")
+
+# num =  "4"+"2"
+# val = int(num)+6
+# print(float(num))       
