ab5c40105ade0a2dc72c264bb93a39ed15d3bc14
Bhushan1834 <bhushantarte44@gmail.com>
2026-04-17 22:57:47 +0530

    Create unary, identity & membership.py
    added

---
+++ unary, identity & membership.py
@@ -0,0 +1,26 @@
+# num = 5
+# print(num<<1)
+
+# num = 14 
+# print(num<<1)
+
+# print(num>>1)
+
+# identity 
+# x = 5
+# y = 5
+# print(x is y)
+
+# arr1 = [1,2,3]
+# arr2 = [1,2,3]
+
+# print(arr1 is arr2)
+# print(arr1 is not arr2 )
+
+#membership
+
+my_list = [1,2,3,4,5,]
+print( 5 in my_list)
+print( 7 in my_list)
+
+print(5 not in my_list )
