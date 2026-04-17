433b26c747ee8e6ad17e72719ef3b7434ee028ef
Bhushan1834 <bhushantarte44@gmail.com>
2026-04-17 22:57:36 +0530

    Create truly and fasly value.py

---
+++ truly and fasly value.py
@@ -0,0 +1,13 @@
+# if 5:
+#     print("Inside if")
+
+if 0 :
+    print("inside if ")
+else :
+    print("Inside else")    
+
+if "" :
+    print("inside if ")
+else :
+    print("Inside else")    
+
